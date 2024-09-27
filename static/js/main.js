6902e149906c8935b4c5ace3e0dd3281beb89a57
mohamed_yehia <@24-mohamedyehia>
2024-09-27 15:59:45 +0300

    update main.js to add reset button

---
+++ static/js/main.js
@@ -0,0 +1,5 @@
+function resetForm() {
+    document.getElementById('reviewTextArea').value = ''; // Clear the textarea
+    document.getElementById('sentimentValue').innerHTML = ''; // Clear only the sentiment result
+
+}
